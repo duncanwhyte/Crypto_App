16f9f8a035b3a29ca80f918b854b0ef7c3b41cd6
Duncan Whyte <duncan_whyte@hotmail.co.uk>
2024-02-20 14:59:36 +0000

    Added some state to the search input need to add coin search functionality

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -1,8 +1,10 @@
 "use client";
 import Link from "next/link";
+import {useState} from "react";
 import {usePathname} from "next/navigation";
 export default function Navbar() {
     const pathName = usePathname();
+    const [coinSearchVal, setCoinSearchVal] = useState("");
     return (
         <nav className="flex justify-between items-center">
             <div className="flex items-center">
@@ -28,14 +30,26 @@ export default function Navbar() {
             </div>
             <div className="flex items-center space-x-4">
             <div className="relative">
-                <input className="inline-block px-12 py-3 bg-[#232334] rounded-xl :focus outline-none" placeholder="Search..." />
+                <input onChange={(e) => setCoinSearchVal(e.target.value)} className="inline-block px-12 py-3 bg-[#232334] rounded-xl :focus outline-none" placeholder="Search..." value={coinSearchVal} />
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5 absolute top-3.5 left-4">
   <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
 </svg>
             </div>
-            <div className="flex justify-center items-center">
+            <div>
+            <div className="min-w-24 relative">
+                <select className="appearance-none bg-[#232334] px-6 py-3 rounded-xl w-full">
+                    <option value="USD">USD</option>
+                    <option value="GBP">GBP</option>
+                    <option value="EUR">EUR</option>
+                    <option value="BTC">BTC</option>
+                    <option value="ETH">ETH</option>
+                </select>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 absolute top-4 right-1">
+  <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+</svg>
             </div>
-            <div className="flex justify-center items-center bg-[#232334] p-2.5 rounded-xl">
+            </div>
+            <div className="flex justify-center items-center bg-[#232334] p-2.5 rounded-xl transition-all hover:bg-[#6161D6] hover:cursor-pointer">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-7 h-7 :hover cursor-pointer">
   <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
 </svg>
