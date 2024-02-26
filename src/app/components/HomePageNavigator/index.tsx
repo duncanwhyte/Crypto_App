e97b01cbe94c080fdbb3a28a813791980bb45c32
Duncan Whyte <duncan_whyte@hotmail.co.uk>
2024-02-26 17:16:39 +0000

    Added styles to coin link and convertor link in the Homepafe Navigator

--- src/app/components/HomePageNavigator/index.tsx
+++ src/app/components/HomePageNavigator/index.tsx
@@ -5,17 +5,11 @@ export default function HomePageNavigator() {
     const pathName = usePathname();
     return (
         <div className="flex gap-5 justify-center">
-            <div>
-                <Link href={"/"} className={`${pathName === "/" ? "bg-[#6161D6]" : "bg-[#232336]"} w-56 py-3 rounded-lg`}>
-                    <button>
+                <Link href={"/"} className={`${pathName === "/" ? "bg-[#6161D6]" : "bg-[#232336]"} flex justify-center w-56 py-3 rounded-lg transition-all hover:scale-110`}>
                     Coins
-                    </button>
                     </Link>
-            </div>
-                <Link href={"/convertor"} className={`${pathName === "/" ? "bg-[#232336]" : "bg-[#6161D6]"} w-56 py-3 rounded-lg`}>
-                <button>
+                <Link href={"/convertor"} className={`${pathName === "/" ? "bg-[#232336]" : "bg-[#6161D6]"} flex justify-center w-56 py-3 rounded-lg transition-all hover:scale-110`}>
                     Convertor
-                </button>
                     </Link>
         </div>
     );
