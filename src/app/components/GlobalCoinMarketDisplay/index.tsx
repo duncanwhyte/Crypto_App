193ad9141032dbd8ba499d6c1dc268c99b334df1
Duncan Whyte <duncan_whyte@hotmail.co.uk>
2024-03-21 14:28:42 +0000

    Added some space between elements

--- src/app/components/GlobalCoinMarketDisplay/index.tsx
+++ src/app/components/GlobalCoinMarketDisplay/index.tsx
@@ -24,7 +24,7 @@ export default function GlobalCoinMarketDisplay() {
     }, [dispatch, currentCurrency]);
     return (
         <div className="bg-[#1E1932] mb-5">
-    <ul className="flex items-center justify-center space-x-5 py-5">
+    <ul className="flex items-center justify-center space-x-8 py-5">
         <li className="flex items-center">
             <Image className="w-5 h-5 mr-1" src={coinsImg} alt="coins-image" />
             Coins {globalData?.active_cryptocurrencies}
