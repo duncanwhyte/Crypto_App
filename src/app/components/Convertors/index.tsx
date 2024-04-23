a0b3ee3e3c2f988962131d69a078f9b5ed4f7d26
Duncan Whyte <duncan_whyte@hotmail.co.uk>
2024-04-23 12:44:17 +0100

    Added a new svg file which will be used to switch the selling and buying coins. Also added functionalty for the conversion of the selling coin to the buying coin and vice versa and coin switch functionality to swap the selling coin and buying coin around.

--- src/app/components/Convertors/index.tsx
+++ src/app/components/Convertors/index.tsx
@@ -6,7 +6,7 @@ import handleCurrencySymbol from "@/app/utils/handleCurrencySymbol";
 import { useState } from "react";
 const selectCurrentCurrency = (state) => state.currentCurrency;
 const selectCoinList = (state) => state.coinList.data;
-export function CoinConvertor({ selling, sellingCoin, buyingCoin}: { selling: boolean , sellingCoin: any , buyingCoin: any}) {
+export function CoinConvertor({ selling, sellingCoin, buyingCoin, sellingAmount, buyingAmount, handleSellingCoin, handleBuyingCoin, handleSellingAmount, handleBuyingAmount}: { selling: boolean , sellingCoin: any , buyingCoin: any, sellingAmount: any, buyingAmount: any,handleSellingCoin: any, handleBuyingCoin: any, handleSellingAmount: any, handleBuyingAmount: any}) {
     const currentCurrency = useAppSelector(selectCurrentCurrency);
     const coinList = useAppSelector(selectCoinList);
     const [showCoinList, setShowCoinList] = useState(false);
@@ -14,27 +14,25 @@ export function CoinConvertor({ selling, sellingCoin, buyingCoin}: { selling: bo
         setShowCoinList(!showCoinList);
     };
     return (
-        <div className="flex flex-col p-6 bg-[#191932] max-w-[50%] rounded-xl">
-        <div>
-        <p>You {selling ? "selling" : "buy"}</p>
-        </div>
+        <div className={`flex flex-col p-6 ${selling ? "bg-[#191932]" : "bg-[#1E1932]"} rounded-xl`}>
+        <>
+        <p className="mb-6 text-[#A7A7CC]">You {selling ? "selling" : "buy"}</p>
+        </>
         <div>
             <div>
-            <div className="flex justify-between items-center relative">
-                <div className={"flex items-center"}>
+            <div className="flex justify-between items-center relative mb-6">
+                <>
+                    <div className="flex items-center">
                 <Image width={24} height={24} src={selling ? sellingCoin?.image : buyingCoin?.image} alt="crypto-image" />
                 <CoinName id={selling ? sellingCoin?.id: buyingCoin?.id} name={selling ? sellingCoin?.name : buyingCoin?.name} symbol={selling ? sellingCoin?.symbol : buyingCoin?.symbol} />
                 <Image onClick={handleShowCoinList} className={"cursor-pointer"} src={priceChangeIcon} width={24} height={24} alt="drop-down-icon"/>
-                </div>
-                <>
-                <input placeholder="" />
+                    </div>
+                <input onChange={selling ? (e) => handleSellingAmount(e, sellingCoin, buyingCoin) :  (e) => handleBuyingAmount(e, buyingCoin, sellingCoin)} className="bg-[#191932] text-end max-w-[40%] focus:outline-0" value={selling ? sellingAmount : buyingAmount}/>
                 </>
-                <ul className="absolute w-full rounded-xl z-10 px-6 top-4 bg-[#232336]">
-                    {showCoinList && coinList.map((coin) => <li key={coin.id}>{coin.name}</li>)}
+                <ul className={`bg-[#232336] absolute w-full rounded-xl z-10 px-6 top-7 transition-all ${showCoinList ? "opacity-100" : "opacity-0"}`}>
+                    {showCoinList && coinList.map((coin) => <li className="cursor-pointer" onClick={() => selling ? handleSellingCoin(coin) : handleBuyingCoin(coin)} key={coin.id}>{coin.name}</li>)}
                 </ul>
             </div>
-            <button>
-            </button>
             <div>
             <hr></hr>
             </div>
--- src/app/convertor/page.tsx
+++ src/app/convertor/page.tsx
@@ -1,18 +1,49 @@
 "use client";
 import HomePageNavigator from "../components/HomePageNavigator";
+import SwitchIcon from "@/app/assets/switchIcon.svg";
 import { useAppSelector, useAppDispatch } from "../lib/hooks";
-import { useEffect} from "react";
+import { useEffect, useState} from "react";
 import { fetchCoinList } from "../lib/features/coinList/coinListSlice";
 import CoinName from "../components/CoinName";
 import TimeDurationSelector from "../components/TimeDurationSelector";
 import { CoinConvertor } from "../components/Convertors";
+import Image from "next/image";
 const selectCoinList = (state) => state.coinList.data;
 const selectCurrentCurrency = (state) => state.currentCurrency;
 export default function Convertor() {
     const coinList = useAppSelector(selectCoinList);
     const currentCurrency = useAppSelector(selectCurrentCurrency);
     const dispatch = useAppDispatch();
+    const [sellingCoin, setSellingCoin] = useState(null);
+    const [buyingCoin, setBuyingCoin] = useState(null);
+    const [sellingAmount, setSellingAmount] = useState(0);
+    const [buyingAmount, setBuyingAmount] = useState(0);
     const newDate = new Date();
+    const handleSellingCoin = (coin) => {
+        setSellingCoin(coin);
+    };
+    const handleBuyingCoin = (coin) => {
+        setBuyingCoin(coin);
+    };
+    const handleCoinSwitch = () => {
+        const sellCoin = sellingCoin || coinList[0];
+        const buyCoin = buyingCoin || coinList[1];
+        const tempAmount = sellingAmount;
+        setSellingCoin(buyCoin);
+        setBuyingCoin(sellCoin);
+        setSellingAmount(buyingAmount);
+        setBuyingAmount(tempAmount);
+    };
+    const handleSellingAmount = (e, sellingCoin, buyingCoin) => {
+        setSellingAmount(e.target.value);
+        const sellingPrice = e.target.value === "" ? 0  : parseFloat(e.target.value) * sellingCoin.current_price;
+        setBuyingAmount(sellingPrice / buyingCoin.current_price);
+    };
+    const handleBuyingAmount = (e, buyingCoin, sellingCoin) => {
+        setBuyingAmount(e.target.value);
+        const buyingPrice = e.target.value === "" ? 0 : parseFloat(e.target.value) * buyingCoin.current_price;
+        setSellingAmount(buyingPrice / sellingCoin.current_price);
+    };
     useEffect(() => {
         dispatch(fetchCoinList());
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -24,17 +55,23 @@ export default function Convertor() {
                 <h3 className="text-xl">Online currency convertor</h3>
                 <p className="text-base text-[]">{newDate.toLocaleDateString()} {Intl.DateTimeFormat("en-GB", {hour: "2-digit", minute: "2-digit"}).format(newDate)}</p>
             </div>
-            <div className="flex flex-col justify-between sm:flex-col md:flex-col lg:flex-row">
-                <CoinConvertor selling={true} sellingCoin={coinList[0]} buyingCoin={false} />
-                <CoinConvertor selling={false} sellingCoin={false} buyingCoin={coinList[1]} />
+            <div className="mb-16 xl:max-w-[1296px] mx-auto">
+                <div className="flex flex-col justify-around sm:flex-col md:flex-row lg:flex-row sm:mx-auto md:mx-auto relative">
+                <CoinConvertor selling={true} sellingCoin={sellingCoin || coinList[0]} buyingCoin={buyingCoin || coinList[1]} sellingAmount={sellingAmount} buyingAmount={buyingAmount} handleSellingCoin={handleSellingCoin} handleBuyingCoin={false} handleSellingAmount={handleSellingAmount} handleBuyingAmount={handleBuyingAmount} />
+                <div className="absolute top-1/2 left-1/2 -ml-[26px] -mt-[26px]">
+                <Image onClick={handleCoinSwitch} className="cursor-pointer" src={SwitchIcon} alt="switch-coin-button" />
+                </div>
+                <CoinConvertor selling={false}  sellingCoin={sellingCoin || coinList[0]} buyingCoin={buyingCoin || coinList[1]} sellingAmount={sellingAmount} buyingAmount={buyingAmount} handleBuyingCoin={handleBuyingCoin} handleSellingCoin={false} handleBuyingAmount={handleBuyingAmount} handleSellingAmount={handleSellingAmount} />
+                </div>
             </div>
             <div>
                 <div className="bg-[#191932] p-6 rounded-xl">
                     <div className="space-x-3">
-                    <CoinName id={coinList[0]?.id} name={coinList[0]?.name} symbol={coinList[0]?.symbol} />
-                    <span>to</span>
-                    <CoinName id={coinList[1]?.id} name={coinList[1]?.name} symbol={coinList[1]?.symbol} />
+                    <CoinName id={sellingCoin?.id || coinList[0]?.id} name={sellingCoin?.name || coinList[0]?.name} symbol={sellingCoin?.symbol || coinList[0]?.symbol} />
+                    <span className="text-[#A7A7CC]">to</span>
+                    <CoinName id={buyingCoin?.id || coinList[1]?.id} name={buyingCoin?.name || coinList[1]?.name} symbol={buyingCoin?.symbol || coinList[1]?.symbol} />
                     </div>
+                    <div></div>
                 </div>
             </div>
             <div>
