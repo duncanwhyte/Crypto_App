260a821f17163f8b79cdc0b42b9ab228f07db0b5
Duncan Whyte <duncan_whyte@hotmail.co.uk>
2024-05-06 13:24:53 +0100

    Added two dates from the coin data we call corresponding to the current currency seleceted. Also added some content which displays the All time low and high prices of the crypto coin called from the api.

--- src/app/coins/[coinId]/page.tsx
+++ src/app/coins/[coinId]/page.tsx
@@ -17,10 +17,14 @@ export default function Coin({ params }: { params: { coinId: string } }) {
     };
     callCoinData();
   }, [params.coinId]);
+  const athDate =
+    coinData && new Date(coinData?.market_data?.ath_date[currentCurrency]);
+  const atlDate =
+    coinData && new Date(coinData?.market_data?.atl_date[currentCurrency]);
   return (
     <main>
-      <div>
-        <div className="bg-[#1E1932] px-8 py-10 rounded-xl">
+      <div className="flex">
+        <div className="bg-[#1E1932] px-8 py-10 rounded-xl w-[40%] gap-8">
           <div className="flex mb-8">
             <Image
               width={48}
@@ -37,43 +41,99 @@ export default function Coin({ params }: { params: { coinId: string } }) {
               </a>
             </div>
           </div>
-          <div>
-            <h2 className="text-2xl">
+          <>
+            <h2 className="text-2xl mb-8">
               {handleCurrencySymbol(currentCurrency)}
               {coinData?.market_data?.current_price[currentCurrency]}
             </h2>
-          </div>
-          <hr></hr>
-          <div className="flex justify-between">
-            <div>
-              <h3>All Time High:</h3>
-            </div>
-            <h3>
-              {handleCurrencySymbol(currentCurrency)}
-              {coinData?.market_data?.ath[currentCurrency]}
-            </h3>
-          </div>
-          <>
-            {new Date(
-              coinData?.market_data?.ath_date[currentCurrency]
-            ).toString()}
           </>
-          <div className="flex justify-between">
-            <div className="flex">
-              <h3>All Time Low:</h3>
+          <hr className="mb-8"></hr>
+          <div className="flex justify-between mb-6">
+            <div className="flex items-center space-x-1">
+              <svg
+                className="self-start"
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M7.16795 3.24807C7.56377 2.65434 8.43623 2.65434 8.83205 3.24808L14.9635 12.4453C15.4066 13.1099 14.9302 14 14.1315 14H1.86852C1.06982 14 0.59343 13.1099 1.03647 12.4453L7.16795 3.24807Z"
+                  fill="#01F1E3"
+                />
+              </svg>
+              <div className="flex flex-col justify-start self-start items-start">
+                <h3 className="text-lg">All Time High:</h3>
+                <p className="text-[#B9B9BA]">
+                  {coinData &&
+                    Intl.DateTimeFormat("en-GB", {
+                      weekday: "short",
+                      day: "2-digit",
+                      year: "numeric",
+                      hour: "2-digit",
+                      minute: "2-digit",
+                      second: "2-digit",
+                      timeZoneName: "short",
+                      month: "short",
+                    }).format(athDate)}
+                </p>
+              </div>
             </div>
-            <h3>
-              {handleCurrencySymbol(currentCurrency)}
-              {coinData?.market_data?.atl[currentCurrency]}
-            </h3>
+            <>
+              <h3>
+                {handleCurrencySymbol(currentCurrency)}
+                {coinData?.market_data?.ath[currentCurrency]}
+              </h3>
+            </>
           </div>
-          <>
-            {new Date(
-              coinData?.market_data?.atl_date[currentCurrency]
-            ).toString()}
-          </>
+          <div className="flex justify-between">
+            <div className="flex items-center space-x-1">
+              <svg
+                className="self-start"
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M8.83205 12.7519C8.43623 13.3457 7.56377 13.3457 7.16795 12.7519L1.03647 3.5547C0.59343 2.89014 1.06982 2 1.86852 2H14.1315C14.9302 2 15.4066 2.89015 14.9635 3.5547L8.83205 12.7519Z"
+                  fill="#FE2264"
+                />
+              </svg>
+              <div className="flex flex-col items-start self-start">
+                <h3 className="text-lg">All Time Low:</h3>
+                <p className="text-[#B9B9BA]">
+                  {coinData &&
+                    Intl.DateTimeFormat("en-GB", {
+                      weekday: "short",
+                      day: "2-digit",
+                      year: "numeric",
+                      hour: "2-digit",
+                      minute: "2-digit",
+                      second: "2-digit",
+                      timeZoneName: "short",
+                      month: "short",
+                    }).format(atlDate)}
+                </p>
+              </div>
+            </div>
+            <>
+              <h3>
+                {handleCurrencySymbol(currentCurrency)}
+                {coinData?.market_data?.atl[currentCurrency]}
+              </h3>
+            </>
+          </div>
         </div>
-        <div></div>
+        <div className="w-[60%]">
+          <p className="text-sm">{coinData?.description?.en}</p>
+        </div>
       </div>
     </main>
   );
