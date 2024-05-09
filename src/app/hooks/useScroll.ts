a65296358ad3e12d2029522b1f56c338617eeb5a
Duncan Whyte <duncan_whyte@hotmail.co.uk>
2024-05-09 13:49:14 +0100

    Added the event listener only when the ref parameter is truthy and also changed the condition in which the cb paramteter is invoked

--- src/app/hooks/useScroll.ts
+++ src/app/hooks/useScroll.ts
@@ -1,31 +1,32 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect} from "react";
+import { useEffect } from "react";
 const handleThrottle = (func, delay: number) => {
-    let timer;
-    return () => {
-      if (!timer) {
-        func();
-        timer = setTimeout(() => {
-          timer = null;
-        }, delay);
-      }
-    };
+  let timer;
+  return () => {
+    if (!timer) {
+      func();
+      timer = setTimeout(() => {
+        timer = null;
+      }, delay);
+    }
   };
+};
 export default function useScroll(ref, cb) {
   useEffect(() => {
-        const handleScroll = () => {
-                if (window.innerHeight + ref.scrollTop >= ref.offsetHeight - 50) {
-                cb();
-                }
-                return;
-            };
-        window.addEventListener("scroll", handleThrottle(handleScroll, 2000));
-        return () => {
-        window.removeEventListener(
+    const handleScroll = () => {
+      const { bottom } = ref.getBoundingClientRect();
+      if (bottom <= window.innerHeight + 100) cb();
+      return;
+    };
+    if (ref) {
+      window.addEventListener("scroll", handleThrottle(handleScroll, 2000));
+    }
+    return () => {
+      window.removeEventListener(
         "scroll",
         handleThrottle(handleScroll, 2000),
         true
-        );
-        };
-        }, [ref]);
+      );
+    };
+  }, [ref]);
 }
