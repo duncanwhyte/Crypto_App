06daa9c3cd3668fcf26cf1272f527cb2a1bbf7b7
Duncan Whyte <duncan_whyte@hotmail.co.uk>
2024-06-25 13:35:24 +0100

    Added some types for the various data structures I will recieve from the API endpoints. e.g. market_data

---
+++ src/app/types/types.ts
@@ -0,0 +1,80 @@
+export interface CoinTableCoin {
+  name: string;
+  id: string;
+  image: string;
+  symbol: string;
+  current_price: number;
+  total_volume: number;
+  market_cap: number;
+  circulating_supply: number;
+  total_supply: number;
+  price_change_percentage_1h_in_currency: number;
+  price_change_percentage_24h_in_currency: number;
+  price_change_percentage_7d_in_currency: number;
+}
+export interface SearchedCoin {
+  id: string;
+  name: string;
+  api_symbol: string;
+  symbol: string;
+  market_cap_rank: number;
+  thumb: string;
+  large: string;
+}
+//// Selected Coin Types
+export interface SelectedCoinData {
+  prices: number[][];
+  total_volumes: number[][];
+}
+export interface SelectedCoin {
+  id: string;
+  name: string;
+  symbol: string;
+  total_volume: number;
+  current_price: number;
+  coinData: SelectedCoinData;
+}
+///// Portfolio Types
+export interface PortfolioCoin {
+  uniqueId: number;
+  id: string;
+  coinAmount: number;
+  purchasedDate: string;
+  name: string;
+  symbol: string;
+  large: string;
+}
+export interface PortfolioSliceState {
+  isLoading: boolean;
+  coins: [];
+  currentDateData: CurrentDateData[];
+  error: boolean;
+}
+export interface CurrentDateData {
+  links: { [key: string]: string[] };
+  description: { [key: string]: string };
+  market_data: MarketData;
+  id: string;
+  image: { [key: string]: string };
+  symbol: string;
+}
+export interface MarketData {
+  ath: { [key: string]: number };
+  atl: { [key: string]: number };
+  ath_date: { [key: string]: string };
+  atl_date: { [key: string]: string };
+  current_price: { [key: string]: number };
+  total_volume: { [key: string]: number };
+  market_cap: { [key: string]: number };
+  price_change_24h: number;
+}
+//// Graph Time Duration Type
+interface GraphTimeDuration {
+  graphTimeDuration: number;
+}
+//// Main Redux Global State Types
+export interface ReduxGlobalState {
+  currentCurrency: string;
+  selectedCoins: SelectedCoin[];
+  graphTimeDuration: GraphTimeDuration;
+}
