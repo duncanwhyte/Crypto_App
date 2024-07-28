ba7a56733d4f85a55a94d7c4afa17eab4e4c1e4f
Duncan Whyte <duncan_whyte@hotmail.co.uk>
2024-07-28 14:17:06 +0100

    Added persisted state only for the portfolio coins and currentCurrency as the theme is persisted by next themes. Added middleware to ignore actions from redux persist when the store is being configured on the server side. Also added a check for the storeProvider to check if the component is being rendered on the server and to render the store without the persist gate and when its client side with the persistor gate.

--- src/app/StoreProvider.tsx
+++ src/app/StoreProvider.tsx
@@ -2,12 +2,27 @@
 import { useRef } from "react";
 import { Provider } from "react-redux";
 import { makeStore, AppStore } from "./lib/store";
-export default function StoreProvider({children}: {
-    children: React.ReactNode
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+export default function StoreProvider({
+  children,
+}: {
+  children: React.ReactNode;
 }) {
-    const storeRef = useRef<AppStore>();
-    if (!storeRef.current) {
-        storeRef.current = makeStore();
-    }
+  const storeRef = useRef<AppStore>();
+  if (!storeRef.current) {
+    storeRef.current = makeStore();
+  }
+  const persistedStore = persistStore(storeRef.current);
+  if (typeof window === "undefined") {
     return <Provider store={storeRef.current}>{children}</Provider>;
+  } else {
+    return (
+      <Provider store={storeRef.current}>
+        <PersistGate loading={null} persistor={persistedStore}>
+          {children}
+        </PersistGate>
+      </Provider>
+    );
+  }
 }
